5391dd6617965c8625f7307950cce22a2f9e8ab5
pandillapujitha <pandillapujitha13@gmail.com>
2026-08-03 14:01:26 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Tourist Guide Portal API is working!"
+    });
+});
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
