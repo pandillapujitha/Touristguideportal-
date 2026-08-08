b5a39e44ce94273f8881fd100a513234ba17cb91
pandillapujitha <pandillapujitha13@gmail.com>
2026-08-08 18:31:00 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,8 @@
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAbRR9xtwBa7Yv93Q7U9UfSd7SqzfOXL6o",
-  authDomain: "touristguideportal.firebaseapp.com",
-  projectId: "touristguideportal",
-  storageBucket: "touristguideportal.appspot.com", // ✅ FIXED
-  messagingSenderId: "928040050366",
-  appId: "1:928040050366:web:d5cb67a371f8f8b82848ef",
-  measurementId: "G-JQRL29FV1L"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  appId: "YOUR_APP_ID"
 };
 
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
